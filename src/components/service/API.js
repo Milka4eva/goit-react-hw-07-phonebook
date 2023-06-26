31f9c1812073f78d0969eaab1bcacbb254163181
Mila Petruk <mila.petruk@gmail.com>
2023-06-26 14:35:03 +0300

    API and final corrections

---
+++ src/components/service/API.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const contactsAPI = axios.create({
+  baseURL: 'https://64931145428c3d2035d14718.mockapi.io/contacts/',
+});
+
+export const getContacts = async () => {
+  const { data } = await contactsAPI.get();
+  return data;
+};
+
+export const deleteContact = async id => {
+  const { data } = await contactsAPI.delete(id);
+  return data;
+};
+
+export const addContact = async contact => {
+  const { data } = await contactsAPI.post('', contact);
+  return data;
+};
